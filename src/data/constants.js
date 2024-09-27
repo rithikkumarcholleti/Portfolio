e732d0933f6fa5e051c315643f63589683d03ae3
Rithik Kumar Cholletti <rithikkumarcholleti@gmail.com>
2024-09-27 18:24:55 +0530

    first changes

---
+++ src/data/constants.js
@@ -0,0 +1,322 @@
+export const Bio = {
+    name: "Rithik Kumar Cholleti",
+    roles: [
+      "Full Stack Developer",
+      "Frontend Developer",
+      "Web Developer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/rithikkumarcholleti",
+    resume:
+      "https://drive.google.com/file/d/1Omu7yuS4tolA3WEv2gtOJk__eL-AIHVE/view?usp=drivesdk",
+    linkedin: "https://www.linkedin.com/in/rithik-cholleti-394973317/",
+    twitter: "https://x.com/RithikKumar25",
+    insta: "https://www.instagram.com/_rithik_09/",
+    facebook: "https://www.facebook.com/rithik.chary/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Redux",
+          image:
+            "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+        },
+        {
+          name: "Next Js",
+          image:
+            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "C++",
+          image: "https://upload.wikimedia.org/wikipedia/commons/1/18/ISO_C%2B%2B_Logo.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+          name: "PHP",
+          image: "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+        {
+          name: "SQL",
+      image: "https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png",
+        },
+      ],
+    },
+    {
+      title: "Web Development",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name: "HTML5",
+          image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "CSS3",
+          image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+        },
+        {
+          name: "PHP",
+          image: "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Git",
+          image: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+        }
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://i.ibb.co/XzTWxnr/orinson.jpg",
+      role: "Frontend Developer Intern",
+      company: "Orinson Technologies",
+      date: "Sept 2024 - Present",
+      desc: "Working on the frontend of the web application using various Frontend technologies.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "NodeJs",
+        "HTML",
+        "CSS",
+        "JavaScript",
+      ],
+    },
+    {
+      id: 1,
+      img: "https://i.ibb.co/QHHD4wW/datapoint.jpg",
+      role: "Python using Machine Learning intern",
+      company: "Datapoint IT & Hardware Tech pvt ltd",
+      date: "Aug 2023 - Feb 2024",
+      desc: "worked on projects using python with machine learning using various python libraries.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Python",
+        "AutoML",
+        "Django",
+        "Pandas",
+        "Numpy",
+        "SQLite",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://i.ibb.co/gjnNMBz/drk.png" ,
+      school: "DRK Institute of Science and Technology",
+      date: "Jun 2020 - Jul 2024",
+      grade: "7.01 CGPA",
+      desc: "I am a Bachelor's degree Graduate in Computer Science and Engineering at DRK Institute of Science and  Technology, Hyderabad. I have completed 4 semesters and have a CGPA of 7.01. I have taken courses in Data Structures, Web Technologies, Object-Oriented Programming, Database Management Systems, Data Analytics, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: "https://i.ibb.co/hHdfKpz/narayana.jpg",
+      school: "Narayana Junior College, Hyderabad",
+      date: "Jun 2018 - Jun 2020",
+      grade: "87%",
+      desc: "I completed my Intermediate at Narayana Junior College, Hyderabad, where I choosed and studied Maths, Physics, Chemistry(MPC). ",
+      degree: "Maths Physics Chemistry (MPC)",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/86nYykb/dilsukhnagar.jpg" ,
+      school: "Dilsukhnagar Public School, Hyderabad",
+      date: "Apr 2017 - Apr 2018",
+      grade: "8.7 CGPA",
+      desc: "I completed my class 10 education at Dilsukhnagar Public School, Hyderabad.",
+      degree: "SSC",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "Weather-Dashboard",
+      date: "Sep 2024",
+      description:
+        "Weather Dashboard 🌤️ is a responsive and dynamic application that provides real-time weather information and a 5-day weather forecast using the OpenWeatherMap API. The dashboard allows users to search for weather details by city and toggle between light and dark modes for an enhanced user experience.",
+      image:
+      "https://github.com/user-attachments/assets/5f5b24c9-be09-4e8c-bc8e-48e16b3b1480",
+      tags: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "React Js",
+        "OpenWeatherMap API",
+        "Redux",
+      ],
+      category: "web app",
+      github: "https://github.com/rithikkumarcholleti/Weather-dashboard",
+      webapp: "https://trackify.duckdns.org",
+    },
+    {
+      id: 0,
+      title: "Product Page",
+      date: "Sep 2024",
+      description:
+        "Developed a Product Page, which is a web application built using React and Redux. It showcases the iPhone 15, allowing users to view product details, add items to their favorites and cart, and navigate through a clean and modern interface.",
+      image:
+        "https://github.com/user-attachments/assets/e0d4eae0-cd5b-4ade-a388-cc3c99182407",
+      tags: ["React Js", "HTML","CSS","Javascript", "Redux"],
+      category: "web app",
+      github: "https://github.com/rithikkumarcholleti/Apple-product-page",
+      webapp: "https://podstream.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "Ecommerce Landing Page",
+      date: "Sep2024",
+      description:
+        "Designed and developed an Ecommerce-Landing-Page which replicates a simple and basic format of an ecommerce website showcasing clothing products with an elegant header , herosection , productpage and a footer which are responsive and dynamic.",
+      image:
+        "https://github.com/user-attachments/assets/ae0ea002-c87f-40f1-b55c-08282333427e",
+      tags: [
+        "React Js",
+        "HTML",
+        "CSS",
+        "Javascript",
+      ],
+      category: "web app",
+      github: "https://github.com/rithikkumarcholleti/ecommerce-landing-page/tree/main/react-landing-page",
+      webapp: "https://vexa-app.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "Salary Predictor using AutoMl",
+      date: "Jun 2023 - aug 2023",
+      description:
+        "Developed an application using AutoML , python and its various libraries which performs salary prediction analysis based on experience and also provides a graphical test set regarding salary vs experience.",
+      image:
+        "https://github.com/user-attachments/assets/c45946ee-bc83-4dd3-9fc9-c7018970e0c6",
+      tags: ["Python", "Numpy", "Pandas", "SQLite", "CSS", "HTML"],
+      category: "machine learning",
+      github: "https://github.com/rithikkumarcholleti/AUTOML",
+      webapp: "https://brain-tumor.netlify.app/",
+    },
+    {
+      id: 4,
+      title: "Currency Converter",
+      date: "Oct 2023",
+      description:
+        "Developed a currency converter application which converts any country currency into request country currency with every different country flags using an extenal api, using which the currency value for every country gets updated time to time.",
+      image:
+        "https://github.com/user-attachments/assets/301f8d02-1d1b-4316-bdb1-449e62f85a37",
+      tags: ["HTML", "CSS", "Javascript", "ExternalAPI"],
+      category: "web app",
+      github: "https://github.com/rithikkumarcholleti/currency-converter/tree/main",
+      webapp: "https://whatsapp-clone-rishav.web.app",
+    },
+    {
+      id: 5,
+      title: "HealthCare Services App",
+      date: "Sep 2024 - Present",
+      description:
+        " Built a Healthcare Services Management Application, is a user-friendly web application designed to provide comprehensive management of healthcare services. The application allows users to view available services, submit service requests, and manage healthcare-related information. With a clean and modern interface, this application aims to enhance the user experience in accessing healthcare services. ",
+      image:
+        "https://github.com/user-attachments/assets/62c35ef5-fa36-4777-a177-b75831ea8629",
+      tags: ["React Js", "Local Storage", "Javascript", "HTML", "CSS"],
+      category: "web app",
+      github: "https://github.com/rithikkumarcholleti/Healthcare-Services",
+      webapp: "https://rishav-react-todo.netlify.app/",
+    },
+    {
+      id: 6,
+      title: "Nike Shoe Spot",
+      date: "Jun 2021",
+      description:
+        "An Ecommerce-Shoe website which replicates various nike shoes with its various models and various colors showcasing a full description about them with a dynamic and responsive code with an elegant Header, Hero-Section, ProductPage and a Footer.",
+      image:
+        "https://github.com/user-attachments/assets/3f24de33-bdaf-4440-ac6d-db6141b97354",
+      tags: ["Javascript", "CSS", "HTML",],
+      category: "web app",
+      github: "https://github.com/rithikkumarcholleti/Nike-Shoe-spot",
+      webapp: "https://breaking-bad-webapp.netlify.app",
+    },
+  ];
+  
+ 
